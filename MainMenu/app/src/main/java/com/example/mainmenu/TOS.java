c78091e0b23e25c526cd2e99b2bdcd1e1cf13f25
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-04-02 17:06:34 +0200

    Added in the TOS of the Techlab

--- MainMenu/app/src/main/java/com/example/mainmenu/TOS.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/TOS.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 public class TOS extends AppCompatActivity{
 
@@ -12,6 +17,27 @@ public class TOS extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tos);
+
+        TextView TOS_textfile = (TextView)findViewById(R.id.tos_text);
+        InputStream inputStream = getResources().openRawResource(R.raw.tosfile);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        String TOS_text = "";
+        int in;
+        try {
+            in = inputStream.read();
+            while (in != -1)
+            {
+                byteArrayOutputStream.write(in);
+                in = inputStream.read();
+            }
+            inputStream.close();
+            TOS_text = byteArrayOutputStream.toString();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        TOS_textfile.setText(TOS_text);
 
         Button back = findViewById(R.id.tos_backtomenu_btn);
         back.setOnClickListener(new View.OnClickListener() {
@@ -24,3 +50,4 @@ public class TOS extends AppCompatActivity{
         });
     }
 }
+
