39a891404a9806a512d6cea6c63e65f8bf93d1ed
Delano Brouwer <Delano.Brouwer@live.nl>
2019-05-10 16:21:26 +0200

    Account Settings Update
    -Added Account Settings screen design.
    -Added Redirect to MyItems screen.

--- MainMenu/app/src/main/java/com/example/mainmenu/AccountSettings.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/AccountSettings.java
@@ -1,7 +1,10 @@
 package com.example.mainmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountSettings extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class AccountSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_settings);
 
+        Button openMyItems = findViewById(R.id.usr_items_btn2);
+
+        openMyItems.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Pressed My Items Button");
+
+                Intent openMyItems = new Intent(getApplicationContext(), MyItems.class);
+                startActivity(openMyItems);
+            }
+        });
     }
 }
