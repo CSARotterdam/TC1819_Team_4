1e477edf4ea70accda2354960a53cc72e0ffaef3
BuildTools <unconfigured@null.spigotmc.org>
2019-05-13 14:02:04 +0200

    Login/Register screen
    All working login and register sytem using firebase

---
+++ MainMenu/app/src/main/java/com/example/mainmenu/RegisterActivity.java
@@ -0,0 +1,97 @@
+package com.example.mainmenu;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userName, userPassword, userEmail;
+    private Button regButton;
+    private TextView userLogin;
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setupUIViews();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()){
+                    //upload data to database
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_password = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "registration succesfull", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity.this, LoginActivityActivity.class));
+                            }else{
+                                Toast.makeText(RegisterActivity.this, "registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+
+                    });
+
+
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent (RegisterActivity.this, LoginActivityActivity.class));
+            }
+        });
+    }
+
+    private void setupUIViews(){
+        userName = (EditText)findViewById(R.id.etUserName);
+        userPassword = (EditText)findViewById(R.id.etUserPassword);
+        userEmail = (EditText)findViewById(R.id.etUserEmail);
+        regButton = (Button)findViewById(R.id.btnRegister);
+        userLogin = (TextView)findViewById(R.id.tvUserLogin);
+    }
+
+
+    private boolean validate(){
+        Boolean result = false;
+
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if(name.isEmpty() || password.isEmpty() || email.isEmpty() ){
+            Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show();
+        }else {
+            result = true;
+        }
+        return result;
+    }
+
+}
+
+
+
