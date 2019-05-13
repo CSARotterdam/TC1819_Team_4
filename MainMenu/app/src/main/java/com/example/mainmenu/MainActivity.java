3a7c45ffc08a1d6b077df1632449ee4345cebc9a
BuildTools <unconfigured@null.spigotmc.org>
2019-05-13 12:26:16 +0200

    Merge branch 'master' of C:\Users\Mitchell\Desktop\school\TC1819_Team_4 with conflicts.

--- MainMenu/app/src/main/java/com/example/mainmenu/MainActivity.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/MainActivity.java
@@ -3,10 +3,18 @@ package com.example.mainmenu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity{
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,9 @@ public class MainActivity extends AppCompatActivity{
         Button openMyItems = findViewById(R.id.usr_items_btn);
         Button openReserveItems = findViewById(R.id.usr_reserve_btn);
         Button openAccountSettings = findViewById(R.id.usr_settings_btn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        logout = (Button)findViewById(R.id.logout_btn);
+
 
         openInventory.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -66,6 +77,16 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(openAccountSettings);
             }
         });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(MainActivity.this, LoginActivityActivity.class));
+            }
+        });
+
     }
 }
 
