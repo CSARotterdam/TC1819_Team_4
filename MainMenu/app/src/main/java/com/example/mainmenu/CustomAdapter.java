adff726dc67636368b448d672f66bdba0b4e9462
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-05-15 07:29:29 +0200

    updates to the inventory (CURRENTLY NOT WORKING)

---
+++ MainMenu/app/src/main/java/com/example/mainmenu/CustomAdapter.java
@@ -0,0 +1,69 @@
+package com.example.mainmenu;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    ArrayList<String> productName;
+    ArrayList<String> product_manufacturer;
+    ArrayList<String> product_id;
+    Context context;
+
+    public CustomAdapter(Context context, ArrayList<String> productName, ArrayList<String> product_manufacturer, ArrayList<String> product_id) {
+        this.context = context;
+        this.productName = productName;
+        this.product_manufacturer = product_manufacturer;
+        this.product_id = product_id;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.name.setText(productName.get(position));
+        holder.manufacturer.setText(product_manufacturer.get(position));
+        holder.prodID.setText(product_id.get(position));
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, productName.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return productName.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, manufacturer, prodID;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name);
+            manufacturer = itemView.findViewById(R.id.manufacturer);
+            prodID = itemView.findViewById(R.id.prodid);
+
+        }
+    }
+}
--- MainMenu/app/src/main/java/com/example/mainmenu/Inventory.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/Inventory.java
@@ -5,13 +5,66 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Inventory extends AppCompatActivity{
+
+    ArrayList<String> productName = new ArrayList<>();
+    ArrayList<String> product_manufacturer = new ArrayList<>();
+    ArrayList<String> product_id = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            JSONArray userArray = obj.getJSONArray("users");
+            for (int i = 0; i < userArray.length(); i++) {
+                JSONObject userDetail = userArray.getJSONObject(i);
+                productName.add(userDetail.getString("Product: "));
+                product_manufacturer.add(userDetail.getString("Made by: "));
+                JSONObject contact = userDetail.getJSONObject("contact");
+                product_id.add(contact.getString("Product Id: "));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter(Inventory.this, productName, product_manufacturer, product_id);
+        recyclerView.setAdapter(customAdapter);
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getResources().openRawResource(R.raw.invelectronics);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
 
     }
 }
