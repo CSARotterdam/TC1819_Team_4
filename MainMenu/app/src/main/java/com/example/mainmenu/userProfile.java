a30b61b2f8239aed3c26c28f940740b485c2c52c
Discopizza <45597024+Discopizza@users.noreply.github.com>
2019-05-19 17:36:07 +0200

    Userinfo
    Changed the accountsettings to show the account information of the person.
    a option to change the info is planned to be made next sprit

--- MainMenu/app/src/main/java/com/example/mainmenu/userProfile.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/userProfile.java
@@ -10,6 +10,9 @@ public class userProfile {
     public String userPhone;
     public String userClass;
 
+    public userProfile(){
+    }
+
     public userProfile(String userName, String userSurname, String userNickName, String userBday, String userEmail, String userID, String userPhone, String userClass) {
         this.userName = userName;
         this.userSurname = userSurname;
@@ -18,6 +21,70 @@ public class userProfile {
         this.userEmail = userEmail;
         this.userID = userID;
         this.userPhone = userPhone;
+        this.userClass = userClass;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
+
+    public String getUserBday() {
+        return userBday;
+    }
+
+    public void setUserBday(String userBday) {
+        this.userBday = userBday;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserClass() {
+        return userClass;
+    }
+
+    public void setUserClass(String userClass) {
         this.userClass = userClass;
     }
 }
