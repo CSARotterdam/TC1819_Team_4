3ce0a7b07de6a67f65a4968583e53be75849b922
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-06-05 21:21:25 +0200

    Tried to make a functioning My Items screen

--- MainMenu/app/src/main/java/com/example/mainmenu/MyItems.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/MyItems.java
@@ -1,14 +1,109 @@
 package com.example.mainmenu;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MyItems extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyItems extends AppCompatActivity{
+
+    private FirebaseDatabase firebaseDatabase;
+    public List<listItem> itemList;
+
+    ArrayList<String> productName = new ArrayList<>();
+    ArrayList<String> product_manufacturer = new ArrayList<>();
+    ArrayList<String> product_id = new ArrayList<>();
+    ArrayList<String> productCategory = new ArrayList<>();
+    ArrayList<String> productTotalStock = new ArrayList<>();
+    ArrayList<String> productCurrentStock = new ArrayList<>();
+    ArrayList<String> productAmountBroken = new ArrayList<>();
+    ArrayList<String> productURL = new ArrayList<>();
+    ArrayList<String> productDescription = new ArrayList<>();
+
+    public String newproductName;
+    public String newproduct_manufacturer;
+    public String newproduct_id;
+    public String newproductCategory;
+    public String newproductTotalStock;
+    public String newproductCurrentStock;
+    public String newproductAmountBroken;
+    public String newproductURL;
+    public String newproductDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_items);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView2);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        DatabaseReference MyRef = firebaseDatabase.getReference(firebaseAuth.getUid()).child("itemsBorrowed");
+        DatabaseReference databaseReference1 = firebaseDatabase.getReference().child("items");
+
+        databaseReference1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    listItem listItem = dataSnapshot1.getValue(listItem.class);
+                    newproductName = listItem.getProductName();
+                    newproduct_manufacturer = listItem.getProduct_manufacturer();
+                    newproduct_id = listItem.getProduct_id();
+                    newproductCategory = listItem.getProductCategory();
+                    newproductTotalStock = listItem.getProductTotalStock();
+                    newproductCurrentStock = listItem.getProductCurrentStock();
+                    newproductAmountBroken = listItem.getProductAmountBroken();
+                    newproductURL = listItem.getProductURL();
+                    newproductDescription = listItem.getProductDescription();
+
+                    productName.add(newproductName);
+                    product_manufacturer.add(newproduct_manufacturer);
+                    product_id.add(newproduct_id);
+                    productCategory.add(newproductCategory);
+                    productTotalStock.add(newproductTotalStock);
+                    productCurrentStock.add(newproductCurrentStock);
+                    productAmountBroken.add(newproductAmountBroken);
+                    productURL.add(newproductURL);
+                    productDescription.add(newproductDescription);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        Button back = findViewById(R.id.mitems_backtomenu_btn);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked!");
+
+                Intent back = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
---
+++ MainMenu/app/src/main/java/com/example/mainmenu/MyItemsAdapter.java
@@ -0,0 +1,91 @@
+package com.example.mainmenu;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MyItemsAdapter extends RecyclerView.Adapter<MyItemsAdapter.MyViewHolder> {
+
+    ArrayList<String> productName;
+    ArrayList<String> product_manufacturer;
+    ArrayList<String> product_id;
+    ArrayList<String> productCategory;
+    ArrayList<String> productTotalStock;
+    ArrayList<String> productCurrentStock;
+    ArrayList<String> productAmountBroken;
+    ArrayList<String> productURL;
+    ArrayList<String> productDescription;
+
+    public Button button;
+
+    //ArrayList<Integer> selectedPositions = new ArrayList<>();
+    //int selectedPosition=-1;
+
+
+    Context context;
+
+    public MyItemsAdapter(Context context, ArrayList<String> productName, ArrayList<String> product_manufacturer, ArrayList<String> product_id, ArrayList<String> productCategory, ArrayList<String> productTotalStock, ArrayList<String> productCurrentStock, ArrayList<String> productAmountBroken, ArrayList<String> productURL, ArrayList<String> productDescription) {
+        this.context = context;
+        this.productName = productName;
+        this.product_manufacturer = product_manufacturer;
+        this.product_id = product_id;
+        this.productCategory = productCategory;
+        this.productTotalStock = productTotalStock;
+        this.productCurrentStock = productCurrentStock;
+        this.productAmountBroken = productAmountBroken;
+        this.productURL = productURL;
+        this.productDescription = productDescription;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.resrowlayout, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        holder.name.setText(productName.get(position));
+        holder.manufacturer.setText(product_manufacturer.get(position));
+        holder.prodID.setText(product_id.get(position));
+
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Removed: " + productName.get(position));
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return productName.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, manufacturer, prodID, button;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.resremovebtn);
+            name = itemView.findViewById(R.id.name);
+            manufacturer = itemView.findViewById(R.id.manufacturer);
+            prodID = itemView.findViewById(R.id.prodid);
+
+        }
+    }
+}
