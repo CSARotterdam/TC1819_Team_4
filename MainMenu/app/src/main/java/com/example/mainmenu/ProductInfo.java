82c3d5a988bd201e8adc294c8d458adbd246ee1a
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-06-11 16:23:39 +0200

    pictures now get displayed when productinfo is opened
    Some minor issues have to get fixed (scaling in bigger pics, pics being taking long to download)

--- MainMenu/app/src/main/java/com/example/mainmenu/ProductInfo.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/ProductInfo.java
@@ -22,12 +22,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,6 +47,7 @@ public class ProductInfo extends AppCompatActivity {
     int currAmount;
     Product testProduct;
     private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +59,7 @@ public class ProductInfo extends AppCompatActivity {
         Intent prodinfo = getIntent();
         String productName = prodinfo.getStringExtra("productName");
         String product_manufacturer = prodinfo.getStringExtra("product_manufacturer");
-        String product_id = prodinfo.getStringExtra("product_id");
+        final String product_id = prodinfo.getStringExtra("product_id");
         //String productCategory = prodinfo.getStringExtra("productCategory");
         //int productTotalStock = prodinfo.getIntExtra("productTotalStock", 0);
         //int productCurrentStock = prodinfo.getIntExtra("productCurrentStock", 0);
@@ -76,13 +82,56 @@ public class ProductInfo extends AppCompatActivity {
         TextView body = findViewById(R.id.itemInfo);
         TextView wiki = findViewById(R.id.MoreInfo);
         TextView manufacturer = findViewById(R.id.itemManufacturer);
-        ImageView exampleImage = findViewById(R.id.imageFile);
         Button reserve = findViewById(R.id.ReserveButton);
         title.setText(testProduct.getName());
         body.setText(testProduct.getDescription());
         manufacturer.setText(product_manufacturer);
-        exampleImage.setImageBitmap(testProduct.getPicture());
         currAmount = testProduct.getCurrentAmount();
+
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        storageReference.child("item-pics/"+product_id+".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String downloadUri = uri.toString();
+                ImageView exampleImage = findViewById(R.id.imageFile);
+                Glide.with(getApplicationContext())
+                        .load(downloadUri)
+                        .into(exampleImage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                storageReference.child("item-pics/"+product_id+".png").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String downloadUri = uri.toString();
+                        ImageView exampleImage = findViewById(R.id.imageFile);
+                        Glide.with(getApplicationContext())
+                                .load(downloadUri)
+                                .into(exampleImage);
+                    }
+            }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        storageReference.child("item-pics/" + product_id + ".jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                String downloadUri = uri.toString();
+                                ImageView exampleImage = findViewById(R.id.imageFile);
+                                Glide.with(getApplicationContext())
+                                        .load(downloadUri)
+                                        .into(exampleImage);
+                            }
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                ImageView exampleImage = findViewById(R.id.imageFile);
+                                exampleImage.setImageBitmap(testProduct.getPicture());
+                            }
+                        });
+                    }});}});
+
         wiki.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
