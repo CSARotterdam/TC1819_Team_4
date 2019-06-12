36c7dd8f277f25a794aabcddcc504e309cfe1b79
MitchellHR <0960144@hr.nl>
2019-06-12 22:37:38 +0200

    Fixed it, again i hope

--- MainMenu/app/src/main/java/com/example/mainmenu/LoginActivityActivity.java
+++ MainMenu/app/src/main/java/com/example/mainmenu/LoginActivityActivity.java
@@ -1,4 +1,3 @@
-
 package com.example.mainmenu;
 
 import android.app.ProgressDialog;
@@ -18,6 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivityActivity extends AppCompatActivity {
 
@@ -27,6 +31,7 @@ public class LoginActivityActivity extends AppCompatActivity {
     private TextView Register;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    private String userRoleAdmin;
 
 
     @Override
@@ -35,9 +40,13 @@ public class LoginActivityActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
         setupUIViews();
 
-        Email =(EditText)findViewById(R.id.etEmail);
-        Password = (EditText)findViewById(R.id.etUserPassword);
-        Login = (Button)findViewById(R.id.btnLogin);
+        Email = (EditText) findViewById(R.id.etEmail);
+        Password = (EditText) findViewById(R.id.etUserPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+        userProfile profile = new userProfile();
+        profile.setUserRole("User");
+        userRoleAdmin = (profile.getUserRole());
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -45,7 +54,7 @@ public class LoginActivityActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(this);
 
-        if(user != null){
+        if (user != null) {
             finish();
             startActivity(new Intent(LoginActivityActivity.this, MainActivity.class));
         }
@@ -53,23 +62,22 @@ public class LoginActivityActivity extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ((Email.getText().toString().isEmpty()) && (Password.getText().toString().isEmpty()) ){
+                if ((Email.getText().toString().isEmpty()) && (Password.getText().toString().isEmpty())) {
                     return;
-                }else{
+                } else {
+
                     validate(Email.getText().toString(), Password.getText().toString());
                 }
             }
         });
 
 
-
-
-       Register.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               startActivity(new Intent (LoginActivityActivity.this, RegisterActivity.class));
-           }
-       });
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivityActivity.this, RegisterActivity.class));
+            }
+        });
 
 
     }
@@ -80,28 +88,56 @@ public class LoginActivityActivity extends AppCompatActivity {
         if ((Email.getText().toString().isEmpty()) || (Password.getText().toString().isEmpty())) {
             Toast.makeText(LoginActivityActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
             progressDialog.dismiss();
-        }else {
+        } else {
             firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            progressDialog.dismiss();
-                            Toast.makeText(LoginActivityActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(LoginActivityActivity.this, MainActivity.class));
-                        } else {
-                            Toast.makeText(LoginActivityActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                            progressDialog.dismiss();
+                    final AuthResult taskResult = task.getResult();
+                    FirebaseUser user = task.getResult().getUser();
+                    String uid = user.getUid();
+                    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = firebaseDatabase.getReference().child("Users").child(uid);
+
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            userProfile profile = dataSnapshot.getValue(userProfile.class);
+                            String role = profile.getUserRole();
+
+                            if (role.equals("Admin")) {
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivityActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                AuthResult test = taskResult;
+                                startActivity(new Intent(LoginActivityActivity.this, MainActivityAdmin.class));
+                            } else {
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivityActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivityActivity.this, MainActivity.class));
+                            }
                         }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Toast.makeText(LoginActivityActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT);
+
+                        }
+                    });
+
+
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivityActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
                     }
+                }
             });
 
-
-    }}
-
-        private void setupUIViews(){
-            Register = (TextView)findViewById(R.id.tvRegister);
         }
+    }
+
+
+    private void setupUIViews() {
+        Register = (TextView) findViewById(R.id.tvRegister);
+    }
 
 }
-
 
